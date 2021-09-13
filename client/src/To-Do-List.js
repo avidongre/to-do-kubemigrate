d1702ad776952dd2b30a485c587056ff26d2451d
avidongre <avidongre@gmail.com>
2021-09-13 14:58:24 +0530

    Update To-Do-List.js

--- client/src/To-Do-List.js
+++ client/src/To-Do-List.js
@@ -148,7 +148,7 @@ class ToDoList extends Component {
       <div>
         <div className="row">
           <Header className="header" as="h2">
-            TO DO LIST ON AKS ON 13-SEP
+            TO DO LIST ON AKS ON 13-SEP - Demo #2
           </Header>
         </div>
         <div className="row">
